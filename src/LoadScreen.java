cf2326995349f995218b20c66bf09472ae6c62a3
Stefan <Stefan@192.168.9.244>
2013-03-07 12:20:36 +0100

    Make loadscreen borderless and add logo

--- src/LoadScreen.java
+++ src/LoadScreen.java
@@ -1,4 +1,5 @@
 import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
@@ -6,10 +7,13 @@ import java.net.URL;
 
 import javax.imageio.ImageIO;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 
 
-public class LoadScreen extends JDialog
+public class LoadScreen extends JFrame
 {
+	Image logo;
+	
 	/**
 	 * 
 	 */
@@ -17,15 +21,11 @@ public class LoadScreen extends JDialog
 
 	public LoadScreen()
 	{
-		Image logo = null;
-		
 		try
 		{
-			File logoFile = new File("C:\\Users\\Stefan\\Desktop\\logo.png");
-
-			logo = ImageIO.read(logoFile);
+			logo = ImageIO.read(new URL("http://i.imgur.com/oeLKY3p.jpg"));
 		}
-		catch (IOException e)
+		catch (Exception e)
 		{
 			e.printStackTrace();
 		}
@@ -33,8 +33,18 @@ public class LoadScreen extends JDialog
 		this.setSize(logo.getWidth(null), logo.getHeight(null));
 		this.setLocationRelativeTo(null);
 		
-		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setUndecorated(true);
 		
-		this.getGraphics().drawImage(logo, 0, 0, this.getWidth(), this.getHeight(), this);
+		this.setVisible(true);
+	}
+	
+	@Override
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+		
+		this.setSize(logo.getWidth(this), logo.getHeight(this));
+		g.drawImage(logo, 0, 0, logo.getWidth(null), logo.getHeight(null), null);
 	}
 }
