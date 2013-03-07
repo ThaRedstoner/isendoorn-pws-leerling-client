3e229616d43c343439f4418bea925a2e90f208b9
Stefan <Stefan@192.168.9.244>
2013-03-07 12:00:48 +0100

    Start work on load screen

--- src/LoadScreen.java
+++ src/LoadScreen.java
@@ -1,4 +1,40 @@
+import java.awt.Frame;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
-public class LoadScreen {
+import javax.imageio.ImageIO;
+import javax.swing.JDialog;
 
+
+public class LoadScreen extends JDialog
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 196991606721642137L;
+
+	public LoadScreen()
+	{
+		Image logo = null;
+		
+		try
+		{
+			File logoFile = new File("C:\\Users\\Stefan\\Desktop\\logo.png");
+
+			logo = ImageIO.read(logoFile);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		this.setSize(logo.getWidth(null), logo.getHeight(null));
+		this.setLocationRelativeTo(null);
+		
+		this.setVisible(true);
+		
+		this.getGraphics().drawImage(logo, 0, 0, this.getWidth(), this.getHeight(), this);
+	}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+import java.awt.Frame;
+import java.awt.Window;
+
 
 public class Main
 {
 	public static void main(String[] args)
 	{
-		
+		LoadScreen load = new LoadScreen();
 	}
 }
